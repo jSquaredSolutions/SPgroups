818ea44aa6bc3ffa49121169aafd00959cced65a
Jsquared <info@jsquaredsolutions.net>
2020-01-28 12:18:02 -0500

    See readme

--- build/index.js
+++ build/index.js
@@ -10,8 +10,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var spauth = __importStar(require("node-sp-auth"));
 var request = __importStar(require("request-promise"));
 var fs = require('fs');
-var fileName = '../file2.json';
-var file2 = require(fileName);
+var fileName2 = '../file2.json';
+var file2 = require(fileName2);
+var fileName = '../file.json';
+var file = require(fileName);
 var credentialOptions = file2;
 var url = "https://sharepointsource123.sharepoint.com/";
 //get auth options
@@ -20,15 +22,46 @@ spauth.getAuth(url, credentialOptions)
     //perform request with any http-enabled library (request-promise in a sample below):
     var headers = options.headers;
     headers['Accept'] = 'application/json;odata=verbose';
+    var sitesArray = [];
+    var sitesArray2 = [];
     request.get({
-        url: 'https://sharepointsource123.sharepoint.com/_api/Web/webs',
+        url: 'https://sharepointsource123.sharepoint.com/sites/testingAR/_api/Web/webs',
         headers: headers
     }).then(function (response) {
         response = JSON.parse(response);
-        for (var Title in response.d.results) {
-            console.log("" + response.d.results[Title].Title);
+        for (var element in response.d.results) {
+            console.log('Subsite: ' + response.d.results[element].Title);
+            file.title.push(response.d.results[element].Title);
+            requestSP(response.d.results[element].Url);
         }
+        fs.writeFile(fileName, JSON.stringify(file), function (err) {
+            if (err)
+                return console.log(err);
+            console.log('writing to ' + fileName);
+        });
     }).catch(function (err) {
         console.log(err);
     });
+    function requestSP(Url) {
+        request.get({
+            url: Url + '/_api/Web/webs',
+            headers: headers
+        }).then(function (response) {
+            response = JSON.parse(response);
+            for (var element in response.d.results) {
+                console.log('Subsite > Subsite: ' + response.d.results[element].Title);
+                file.title.push(response.d.results[element].Title);
+                requestSP(response.d.results[element].Url);
+            }
+            ;
+            fs.writeFile(fileName, JSON.stringify(file), function (err) {
+                if (err)
+                    return console.log(err);
+                console.log('writing to ' + fileName);
+            });
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+    ;
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as spauth from 'node-sp-auth';
 import * as request from 'request-promise';
 var fs = require('fs');
-var fileName = '../file2.json';
-var file2 = require(fileName);
+var fileName2 = '../file2.json';
+var file2 = require(fileName2);
+var fileName = '../file.json';
+var file = require(fileName);
 
 let credentialOptions = file2;
 let url = "https://sharepointsource123.sharepoint.com/";
@@ -15,15 +17,45 @@ spauth.getAuth(url, credentialOptions)
     let headers = options.headers;
     headers['Accept'] = 'application/json;odata=verbose';
 
+    var sitesArray = [];
+    let sitesArray2 = [];
+
     request.get({
-      url: 'https://sharepointsource123.sharepoint.com/_api/Web/webs',
+      url: 'https://sharepointsource123.sharepoint.com/sites/testingAR/_api/Web/webs',
       headers: headers
     }).then(response => {
-      response = JSON.parse(response);
-      for (const Title in response.d.results) {
-        console.log(`${response.d.results[Title].Title}`)
+      response = JSON.parse(response)
+      for (const element in response.d.results) {
+        console.log('Subsite: ' + response.d.results[element].Title);
+        file.title.push(response.d.results[element].Title);
+        requestSP(response.d.results[element].Url);
       }
+      fs.writeFile(fileName, JSON.stringify(file), function (err) {
+        if (err) return console.log(err);
+        console.log('writing to ' + fileName);
+      });
     }).catch(err => {
       console.log(err)
     })
+
+    function requestSP(Url) {
+      request.get({
+        url: Url + '/_api/Web/webs',
+        headers: headers
+      }).then(response => {
+        response = JSON.parse(response);
+        for (const element in response.d.results) {
+          console.log('Subsite > Subsite: ' + response.d.results[element].Title);
+          file.title.push(response.d.results[element].Title);
+          requestSP(response.d.results[element].Url);
+        };
+        fs.writeFile(fileName, JSON.stringify(file), function (err) {
+          if (err) return console.log(err);
+          console.log('writing to ' + fileName);
+        });
+      }).catch(err => {
+        console.log(err)
+      });
+    };
   });
+
